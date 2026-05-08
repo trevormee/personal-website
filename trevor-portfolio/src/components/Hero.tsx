71f5cf5bf9e86fd358ce18c335a4935f028449cd
Trevor Mee <122403539+trevormee@users.noreply.github.com>
2026-05-08 14:24:58 +0000

    add background image to Hero section

--- trevor-portfolio/src/components/Hero.tsx
+++ trevor-portfolio/src/components/Hero.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+import heroBackgroundImage from "../assets/hero-background-1.jpg";
+
 export default function Hero() {
-    return (
-        <section id="hero">
-            <h2>Hero</h2>
-        </section>
-    )
+  return (
+    <section
+      id="hero"
+      className="hero-section"
+      style={{ backgroundImage: `url(${heroBackgroundImage})` }}
+    >
+      <div className="hero-content">
+        <h1>Trevor Mee</h1>
+        <h2>Software Engineer | M.S. Analytics Candidate</h2>
+      </div>
+    </section>
+  );
 }
