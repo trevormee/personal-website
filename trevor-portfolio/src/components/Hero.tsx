0d16023bd69c7978b7c58b2b4518a2e1f6d0d6b3
Trevor Mee <122403539+trevormee@users.noreply.github.com>
2026-05-08 16:08:21 +0000

    add mui dependency, add github and linkedln buttons to hero section

--- trevor-portfolio/src/components/Hero.tsx
+++ trevor-portfolio/src/components/Hero.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import heroBackgroundImage from "../assets/blackCircuit.avif";
 // import heroBackgroundImage from "../assets/futureRing.webp";
 // import heroBackgroundImage from "../assets/futureTunnel.avif";
 // import heroBackgroundImage from "../assets/hero-background.jpg";
+import githubLogo from "../assets/hero-images/github-logo.png";
+import linkedinLogo from "../assets/hero-images/linkedin-logo.jpg";
+import emailLogo from "../assets/hero-images/email-logo.webp";
 
 export default function Hero() {
   return (
@@ -14,6 +19,26 @@ export default function Hero() {
       <div className="hero-content">
         <h1>Trevor Mee</h1>
         <h2>Software Engineer | M.S. Analytics Candidate</h2>
+
+        <Stack direction="row" spacing={2} className="hero-buttons">
+          <Button
+            variant="text"
+            href="https://github.com/trevormee"
+            target="_blank"
+            rel="noopener noreferrer"
+            startIcon={<img src={githubLogo} alt="" className="button-logo" />}
+          ></Button>
+
+          <Button
+            variant="text"
+            href="https://www.linkedin.com/in/trevormee/"
+            target="_blank"
+            rel="noopener noreferrer"
+            startIcon={
+              <img src={linkedinLogo} alt="" className="button-logo" />
+            }
+          ></Button>
+        </Stack>
       </div>
     </section>
   );
