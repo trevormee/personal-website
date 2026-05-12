12b492fedc808bb31f8e47214d9669d3b9a87fd5
Trevor Mee <tvm5@students.uwf.edu>
2026-05-11 19:31:44 -0500

    experience section

--- trevor-portfolio/src/components/Experience.tsx
+++ trevor-portfolio/src/components/Experience.tsx
@@ -4,6 +4,7 @@ import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
+import { Box, Container, Paper, Stack, Typography } from "@mui/material";
 
 type WorkExperience = {
   company: string;
@@ -13,7 +14,7 @@ type WorkExperience = {
   bullets: string[];
 };
 
-const CONST_WORK_EXPERIENCE: WorkExperience[] = [
+const WORK_EXPERIENCE: WorkExperience[] = [
   // DS2
   {
     company: "DS2",
@@ -28,29 +29,76 @@ const CONST_WORK_EXPERIENCE: WorkExperience[] = [
   },
 
   // ASEI
+  {
+    company: "ASEI",
+    title: "Software Engineer Intern",
+    location: "Niceville, FL",
+    dates: "Summers 2023 & 2024",
+    bullets: [
+      "Collabored with 7 interns to build an optimal tracker system with custom-embedded software",
+      "Built an automated calibration tool and designed GUI components",
+      "Collaborated with the DevOps team to enhance software automation process",
+    ],
+  },
 ];
 
 export default function Experience() {
   return (
     <section id="experience">
-      <h2 style={{ textAlign: "center", marginTop: "2rem" }}>Experience</h2>
+      <h2 style={{ textAlign: "center" }}>Experience</h2>
 
-      <Timeline>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Software Engineer I</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Software Enginner Intern</TimelineContent>
-        </TimelineItem>
-      </Timeline>
+      <Container
+        maxWidth="lg"
+        sx={{
+          px: { xs: 2, md: 4 },
+          py: 0,
+        }}
+      >
+        <Timeline position="right">
+          {WORK_EXPERIENCE.map((job, index) => (
+            <TimelineItem key={job.title}>
+              <TimelineSeparator>
+                <TimelineDot variant="outlined" />
+                {index < WORK_EXPERIENCE.length - 1 && <TimelineConnector />}
+              </TimelineSeparator>
+              <TimelineContent>
+                <Paper variant="outlined">
+                  <Stack spacing={2}>
+                    <Box>
+                      <Typography variant="h5" color="text.primary">
+                        {job.company}
+                      </Typography>
+
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          fontStyle: "italic",
+                          color: "text.secondary",
+                        }}
+                      >
+                        {job.title} | {job.location} | {job.dates}
+                      </Typography>
+                    </Box>
+
+                    <Box component="ul" sx={{ m: 0, pl: 3 }}>
+                      {job.bullets.map((bullet) => (
+                        <Typography
+                          key={bullet}
+                          component="li"
+                          variant="body1"
+                          sx={{ mb: 1 }}
+                        >
+                          {bullet}
+                        </Typography>
+                      ))}
+                    </Box>
+                  </Stack>
+                </Paper>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+        </Timeline>
+      </Container>
     </section>
   );
 }
